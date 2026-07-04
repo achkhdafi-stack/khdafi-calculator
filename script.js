8f566b6813438e447da9db9524b5481f07ec03bb
achkhdafi-stack <ach.khdafi@gmail.com>
2026-07-04 11:43:46 +0700

    first calculator commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function appendValue(value) {
+    const display = document.getElementById('display');
+    if (display.value === '0' || display.value === 'Error') {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '0';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+
+    if (display.value.length > 1) {
+        display.value = display.value.slice(0, -1);
+    } else {
+        display.value = '0';
+    }
+}
+
+//Initial value
+    document.getElementById('display').value = '0';
